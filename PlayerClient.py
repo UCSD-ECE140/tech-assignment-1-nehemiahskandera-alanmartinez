bdc9b4a5c792c40b12c6ab4bfa83f8c89b80b042
alanemar <81992498+alanemar@users.noreply.github.com>
2024-04-14 11:57:41 -0700

    Update PlayerClient.py

--- PlayerClient.py
+++ PlayerClient.py
@@ -5,6 +5,8 @@ from dotenv import load_dotenv
 import paho.mqtt.client as paho
 from paho import mqtt
 import time
+import random
+
 
 
 # setting callbacks for different events to see if it works, print the message etc.
@@ -56,6 +58,11 @@ def on_message(client, userdata, msg):
 
     print("message: " + msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
 
+def publish_rand_data(client, topic):
+    while True:
+        random_data = random.randint(0, 100)
+        client.publish(topic, random_data)
+        time.sleep(3)
 
 if __name__ == '__main__':
     load_dotenv(dotenv_path='./credentials.env')
@@ -66,7 +73,8 @@ if __name__ == '__main__':
     password = os.environ.get('PASSWORD')
 
     client = paho.Client(callback_api_version=paho.CallbackAPIVersion.VERSION1, client_id="Player1", userdata=None, protocol=paho.MQTTv5)
-    
+    client2 = paho.Client(callback_api_version=paho.CallbackAPIVersion.VERSION1, client_id="Player2", userdata=None, protocol=paho.MQTTv5)
+
     # enable TLS for secure connection
     client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
     # set username and password
@@ -78,6 +86,21 @@ if __name__ == '__main__':
     client.on_subscribe = on_subscribe # Can comment out to not print when subscribing to new topics
     client.on_message = on_message
     client.on_publish = on_publish # Can comment out to not print when publishing to topics
+
+        # enable TLS for secure connection
+    client2.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
+    # set username and password
+    client2.username_pw_set(username, password)
+    # connect to HiveMQ Cloud on port 8883 (default for MQTT)
+    client2.connect(broker_address, broker_port)
+
+    # setting callbacks, use separate functions like above for better visibility
+    client2.on_subscribe = on_subscribe # Can comment out to not print when subscribing to new topics
+    client2.on_message = on_message
+    client2.on_publish = on_publish # Can comment out to not print when publishing to topics
+
+    client.loop_start()
+    client2.loop_start()
 
     lobby_name = "TestLobby"
     player_1 = "Player1"
@@ -107,5 +130,26 @@ if __name__ == '__main__':
     client.publish(f"games/{lobby_name}/{player_3}/move", "DOWN")
     client.publish(f"games/{lobby_name}/start", "STOP")
 
+ 
+    topic_client1 = "client1/random_data"
+    topic_client2 = "client2/random_data"
 
-    client.loop_forever()
+    client.subscribe(topic_client1)
+    client2.subscribe(topic_client2)
+    last_time = time.time()
+    while True:
+        client.loop()
+        client2.loop()
+        
+        # Check if 3 seconds have passed
+        if time.time() - last_time >= 3:
+            # Publish random data from both clients
+            random_data_1 = random.randint(0, 100)
+            random_data_2 = random.randint(0, 100)
+            client.publish(topic_client1, random_data_1)
+            client2.publish(topic_client2, random_data_2)
+            print(f"Published {random_data_1} to {topic_client1}")
+            print(f"Published {random_data_2} to {topic_client2}")
+            
+            # Reset the timer
+            last_time = time.time()
