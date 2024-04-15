cefde2024d693be30e53cbd86f69e79fe02ca422
alanemar <81992498+alanemar@users.noreply.github.com>
2024-04-14 23:13:54 -0700

    Update PlayerClient.py

--- PlayerClient.py
+++ PlayerClient.py
@@ -5,8 +5,6 @@ from dotenv import load_dotenv
 import paho.mqtt.client as paho
 from paho import mqtt
 import time
-import random
-
 
 
 # setting callbacks for different events to see if it works, print the message etc.
@@ -20,6 +18,9 @@ def on_connect(client, userdata, flags, rc, properties=None):
         :param properties: can be used in MQTTv5, but is optional
     """
     print("CONNACK received with code %s." % rc)
+    client.subscribe(f"games/{lobby_name}/lobby")
+    client.subscribe(f"games/{lobby_name}/{player_name}/game_state")
+    client.subscribe(f"games/{lobby_name}/scores")
 
 
 # with this callback you can see if your publish was successful
@@ -58,11 +59,6 @@ def on_message(client, userdata, msg):
 
     print("message: " + msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
 
-def publish_rand_data(client, topic):
-    while True:
-        random_data = random.randint(0, 100)
-        client.publish(topic, random_data)
-        time.sleep(3)
 
 if __name__ == '__main__':
     load_dotenv(dotenv_path='./credentials.env')
@@ -73,83 +69,56 @@ if __name__ == '__main__':
     password = os.environ.get('PASSWORD')
 
     client = paho.Client(callback_api_version=paho.CallbackAPIVersion.VERSION1, client_id="Player1", userdata=None, protocol=paho.MQTTv5)
-    client2 = paho.Client(callback_api_version=paho.CallbackAPIVersion.VERSION1, client_id="Player2", userdata=None, protocol=paho.MQTTv5)
-
+    
     # enable TLS for secure connection
     client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
     # set username and password
     client.username_pw_set(username, password)
     # connect to HiveMQ Cloud on port 8883 (default for MQTT)
-    client.connect(broker_address, broker_port)
-
-    # setting callbacks, use separate functions like above for better visibility
-    client.on_subscribe = on_subscribe # Can comment out to not print when subscribing to new topics
-    client.on_message = on_message
-    client.on_publish = on_publish # Can comment out to not print when publishing to topics
-
-        # enable TLS for secure connection
-    client2.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
-    # set username and password
-    client2.username_pw_set(username, password)
-    # connect to HiveMQ Cloud on port 8883 (default for MQTT)
-    client2.connect(broker_address, broker_port)
-
-    # setting callbacks, use separate functions like above for better visibility
-    client2.on_subscribe = on_subscribe # Can comment out to not print when subscribing to new topics
-    client2.on_message = on_message
-    client2.on_publish = on_publish # Can comment out to not print when publishing to topics
-
-    client.loop_start()
-    client2.loop_start()
-
-    lobby_name = "TestLobby"
-    player_1 = "Player1"
-    player_2 = "Player2"
-    player_3 = "Player3"
-
-    client.subscribe(f"games/{lobby_name}/lobby")
-    client.subscribe(f'games/{lobby_name}/+/game_state')
-    client.subscribe(f'games/{lobby_name}/scores')
-
-    client.publish("new_game", json.dumps({'lobby_name':lobby_name,
-                                            'team_name':'ATeam',
-                                            'player_name' : player_1}))
     
-    client.publish("new_game", json.dumps({'lobby_name':lobby_name,
-                                            'team_name':'BTeam',
-                                            'player_name' : player_2}))
-    
-    client.publish("new_game", json.dumps({'lobby_name':lobby_name,
-                                        'team_name':'BTeam',
-                                        'player_name' : player_3}))
+# Assign the callback functions
+client.on_connect = on_connect
+client.on_message = on_message
+client.on_publish = on_publish
+client.on_subscribe = on_subscribe
 
-    time.sleep(1) # Wait a second to resolve game start
-    client.publish(f"games/{lobby_name}/start", "START")
-    client.publish(f"games/{lobby_name}/{player_1}/move", "UP")
-    client.publish(f"games/{lobby_name}/{player_2}/move", "DOWN")
-    client.publish(f"games/{lobby_name}/{player_3}/move", "DOWN")
-    client.publish(f"games/{lobby_name}/start", "STOP")
+# Connect to the MQTT broker
+client.connect(broker_address, broker_port)
 
- 
-    topic_client1 = "client1/random_data"
-    topic_client2 = "client2/random_data"
+# Start the loop to process received messages
+client.loop_start()
 
-    client.subscribe(topic_client1)
-    client2.subscribe(topic_client2)
-    last_time = time.time()
+# Game setup variables
+lobby_name = "TestLobby"  # This should be unique to your session
+player_name = "Player1"  # This should be unique to your player
+
+# Publish the new game command to join the lobby
+client.publish("new_game", json.dumps({
+    'lobby_name': lobby_name,
+    'team_name': 'ATeam',
+    'player_name': player_name
+}), qos=1)
+
+# Wait a second for the new game command to be processed by the server
+time.sleep(1)
+
+# Publish a start command to begin the game
+client.publish(f"games/{lobby_name}/start", "START", qos=1)
+
+# Allow the player to make moves based on user input
+try:
     while True:
-        client.loop()
-        client2.loop()
-        
-        # Check if 3 seconds have passed
-        if time.time() - last_time >= 3:
-            # Publish random data from both clients
-            random_data_1 = random.randint(0, 100)
-            random_data_2 = random.randint(0, 100)
-            client.publish(topic_client1, random_data_1)
-            client2.publish(topic_client2, random_data_2)
-            print(f"Published {random_data_1} to {topic_client1}")
-            print(f"Published {random_data_2} to {topic_client2}")
-            
-            # Reset the timer
-            last_time = time.time()
+        move_command = input("Enter your move (UP, DOWN, LEFT, RIGHT, or EXIT to quit): ").upper()
+        if move_command in ["UP", "DOWN", "LEFT", "RIGHT"]:
+            client.publish(f"games/{lobby_name}/{player_name}/move", move_command, qos=1)
+        elif move_command == "EXIT":
+            print("Exiting game.")
+            break
+        else:
+            print("Invalid command.")
+except KeyboardInterrupt:
+    print("Game interrupted by user.")
+
+# Stop the loop and disconnect
+client.loop_stop()
+client.disconnect()
